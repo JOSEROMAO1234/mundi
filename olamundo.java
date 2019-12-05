4352a7001590aaf0c1cfc38b478fd39e77ce956d
JOSEROMAO1234 <58479396+JOSEROMAO1234@users.noreply.github.com>
2019-12-05 08:34:16 +0200

    Add files via upload

---
+++ olamundo.java
@@ -0,0 +1,16 @@
+package romao.olamundo;
+
+public class olamundo {
+    
+        public static void main(String[] args){
+          
+            String  a = "Ola Mundo " ;
+            int b = 2019;
+            
+              System.out.println(a + b);
+              System.out.println("Ola Mundo "+b);
+              System.out.println("Ola Mundo 2019");
+    
+    }
+    
+}
